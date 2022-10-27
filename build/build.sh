eb88f073fd387d548d829530ba5519ce874c758d
Gleb Belyakov <rem@mwnt.ru>
2022-10-27 12:59:03 +0300

    switch port to 80

--- build/build.sh
+++ build/build.sh
@@ -5,3 +5,6 @@ tar -cvf app.tar -C ../app/ . --exclude-from=../.gitignore
 docker build -f Dockerfile -t mw/testwork:master . 
 
 rm -rf app.tar
+
+docker tag mw/testwork:master remidor/private:testwork
+docker push remidor/private:testwork
